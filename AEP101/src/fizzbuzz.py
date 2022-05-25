a5858639feb3672ea0da204dcc7435a22870c982
zhiheng.ma <zhiheng.ma@thoughtworks.com>
2022-05-25 23:25:02 +0800

    test report Whizz and Buzz

--- AEP101/src/fizzbuzz.py
+++ AEP101/src/fizzbuzz.py
@@ -6,9 +6,18 @@ class FizzBuzzWhizz:
     def game_num(self):
         if self.is_divisible_by_3():
             return "Fizz"
+        if self.is_divisible_by_5():
+            return "Buzz"
+        if self.is_divisible_by_7():
+            return "Whizz"
         return self.game_num
 
     def is_divisible_by_3(self):
         return self.number % 3 == 0
 
+    def is_divisible_by_5(self):
+        return self.number % 5 == 0
+
+    def is_divisible_by_7(self):
+        return self.number % 7 == 0
 
--- AEP101/tests/unit/test_FizzBuzzGame.py
+++ AEP101/tests/unit/test_FizzBuzzGame.py
@@ -6,6 +6,13 @@ def test_should_report_Fizz_when_reporting_given_mod_by_3():
     fbw = FizzBuzzWhizz(int(12))
     assert fbw.game_num() == "Fizz"
 
+def test_should_report_Buzz_when_reporting_given_mod_by_5():
+    fbw = FizzBuzzWhizz(20)
+    assert fbw.game_num() == "Buzz"
+
+def test_should_report_Whizz_when_reporting_given_mod_by_7():
+    fbw = FizzBuzzWhizz(49)
+    assert fbw.game_num() == "Whizz"
 
 
 
